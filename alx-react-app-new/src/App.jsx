22df3e5bf112f5049afc1810f427f6c462461622
csamii <csamii27@gamil.com>
2025-07-16 21:07:40 +0100

    add styling in react

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <UserProfile
+        name="Alice"
+        age="johmk"
+        bio="Loves hiking and photography"
+      />
+      <Footer />
+    </>
+  )
+}
+
+export default App
