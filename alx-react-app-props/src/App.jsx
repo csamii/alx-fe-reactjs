fcd1b7c0a39284c9f40d3752a9856d2714cbb2a5
csamii <csamii27@gamil.com>
2025-07-18 08:48:52 +0100

    passing userData as userContext provider

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,9 +1,13 @@
 import ProfilePage from './components/ProfilePage';
+import { UserContext } from '.components/UserContext';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-  return <ProfilePage userData={userData} />;
+  return 
+  <UserContext.Provider>
+    <ProfilePage value={userData} />;
+  </UserContext.Provider>
 }
 
 export default App;
