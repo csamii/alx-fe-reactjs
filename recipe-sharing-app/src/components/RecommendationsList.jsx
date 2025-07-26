826ed6bf3ea2e1f7de7574580a7beae221b9218f
csamii <csamii27@gamil.com>
2025-07-26 23:17:47 +0100

    favorite and recommendation

--- recipe-sharing-app/src/components/RecommendationsList.jsx
+++ recipe-sharing-app/src/components/RecommendationsList.jsx
@@ -1,9 +1,17 @@
 import { useRecipeStore } from "./recipeStore"
 
 const RecommendationsList = () => {
-    
+    const recommendations = useRecipeStore(state => state.recommendations);
+
   return (
-    <div>RecommendationsList</div>
+    <div>
+        {recommendations.map(recommendation => (
+            <div key={recommendation.id}>
+                <h3>{recommendation.title}</h3>
+                <p>{recommendation.description}</p>
+            </div>
+        ))}
+    </div>
   )
 }
 
