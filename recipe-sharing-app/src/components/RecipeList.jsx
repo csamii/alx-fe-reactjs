32235feb43413b2c21bd3d69e304bca05e26f0f7
csamii <csamii27@gamil.com>
2025-08-02 09:26:23 +0100

    updated app.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -8,11 +8,25 @@ const RecipeList = () => {
     const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
     const searchTerm = useRecipeStore(state => state.searchTerm);
     const favorites = useRecipeStore(state => state.favorites);
+    const addFavorite = useRecipeStore(state => state.addFavorite);
+    const removeFavorite = useRecipeStore(state => state.removeFavorite);
+
     console.log(favorites)
-    const toggleFavorite = useRecipeStore(state => state.toggleFavorite);
+    const isFavorite = (id) => favorites.some(fav => fav.id === id);
+
+    {/* Display recipe based on search term */}
     const recipesToDisplay = searchTerm.trim() === ''
         ? recipes
         : filteredRecipes;
+
+    {/* Toggle add and remove favorite */}
+    const toggleFavorite = (recipe) => {
+        if (isFavorite(recipe.id)) {
+            removeFavorite(recipe.id);
+        } else {
+            addFavorite(recipe);
+        }
+    };
 
     return (
         <div>
@@ -24,28 +38,16 @@ const RecipeList = () => {
                 </div>
             ))}*/}
             {recipesToDisplay.map(recipe => {
-                const isFavorited = favorites.includes(recipe.id);
-
                 return (
                     <div key={recipe.id}>
                         <h3><Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link></h3>
                         <p>{recipe.description}</p>
-                        <button onClick={() => toggleFavorite(recipe.id)}>
-                            {isFavorited ? '💖 Unfavorite' : '🤍 Favorite'}
+                        <button onClick={() => toggleFavorite(recipe)}>
+                            {isFavorite(recipe.id) ? 'Remove from Favorites' : 'Add to Favorites'}
                         </button>
                     </div>
                 );
             })}
-            {/*
-            {recipesToDisplay.map(recipe => (
-                <div key={recipe.id}>
-                    <h3><Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link></h3>
-                    <p>{recipe.description}</p>
-                    <button onClick={() => toggleFavorite(recipe.id)}>
-                        {isFavorited ? '💖 Unfavorite' : '🤍 Favorite'}
-                    </button>
-                </div>
-            ))} */}
         </div>
     );
 };
