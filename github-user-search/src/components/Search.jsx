6c58eb4444af38ab17b899cb118d4b95b3911e7e
csamii <csamii27@gamil.com>
2025-08-03 18:12:29 +0100

    update on search file

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -19,7 +19,7 @@ const Search = () => {
             console.log(data)
             setUserData(data || []);
         } catch (err) {
-            setError("Looks like we can't find the user");
+            setError("Looks like we cant find the user");
         } finally {
             setLoading(false);
         }
