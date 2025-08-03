834cbe94adb2d61ba177136b9628c0c66e56e09e
csamii <csamii27@gamil.com>
2025-08-03 06:45:17 +0100

    Configure Environment for API Calls

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,6 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sample from "./components/Sample";
-
+import {BrowserRouter, Routes, Route} from "react-router"
 
 
 function App() {
