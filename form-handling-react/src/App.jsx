9ce9480877bf8faf4da2dcae7b8ffab41c98bc0e
csamii <csamii27@gmail.com>
2025-09-06 12:47:53 +0100

    Formik

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,6 +1,6 @@
 
 import RegistrationForm from "./components/RegistrationForm";
-import FormikForm from "./components/formikForm";
+import FormikForm from "./components/FormikForm";
 
 function App() {
 
