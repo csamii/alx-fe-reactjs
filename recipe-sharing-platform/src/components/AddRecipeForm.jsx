51263a85ea0265b3b23c1b4983c3afb3f4ce651c
csamii <csamii27@gmail.com>
2026-02-27 12:40:13 +0100

    added a new add recipe file

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -5,12 +5,14 @@ import {useRecipeStore} from './recipeStore';
 const AddRecipeForm = () => {
     const addRecipe = useRecipeStore(state => state.addRecipe);
     const [title, setTitle] = useState('');
+    const [ingredients, setIngredients] = useState('');
     const [description, setDescription] = useState('');
 
     const handleSubmit = (event) => {
         event.preventDefault();
         addRecipe({ id: Date.now(), title, description });
         setTitle('');
+        setIngredients('');
         setDescription('');
     };
 
